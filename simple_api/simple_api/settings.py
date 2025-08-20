e2137940120ef9cd29c307dd0a376be662f4ecd6
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-20 16:58:22 +0530

    .env implementation done

--- simple_api/simple_api/settings.py
+++ simple_api/simple_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uq3j6f^0!f%=am1m5h)p4v@3%4!7q4_0*_g*@1m1kja#=&r4z&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
